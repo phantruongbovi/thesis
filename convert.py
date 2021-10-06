1a56175a32a85429f584bcf8e6a93dbf4181cd65
phantruongbovi <44855998+phantruongbovi@users.noreply.github.com>
2021-10-06 09:51:20 +0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -5,15 +5,15 @@ with open('mycsv1.csv', 'w', newline='') as f:
     fieldnames = ['Time', 'Car', 'Motobike', 'Truck', 'Bicycle', 'Total']
     thewriter = csv.DictWriter(f, fieldnames=fieldnames)
     thewriter.writeheader()
-    f = open('23092021/1/end.json')
+    f = open('23092021/4/end.json')
     data = json.load(f)
     for i in data:
         thewriter.writerow({
             'Time' : i,
-            'Car': data[i]['Car'],
-            'Motobike': data[i]['Motobike'],
-            'Truck' : data[i]['Truck'],
-            'Bicycle' : data[i]['Bicycle'],
-            'Total' : data[i]['Total']
+            'Car': int(float(data[i]['Car'])*100),
+            'Motobike': int(float(data[i]['Motobike'])*100),
+            'Truck' : int(float(data[i]['Truck'])*100),
+            'Bicycle' : int(float(data[i]['Bicycle'])*100),
+            'Total' : int(float(data[i]['Total'])*100)
         })
     f.close()
