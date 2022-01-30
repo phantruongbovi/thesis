7513f906597b01b63113dd60b53f350651e77aa5
phantruongbovi <44855998+phantruongbovi@users.noreply.github.com>
2022-01-29 16:34:38 -0800

    Add files via upload

--- cropI.py
+++ cropI.py
@@ -1,17 +1,16 @@
-
 import numpy as np
 import cv2
+import glob
 
-
-count = 1
-while count <= 271:
-    img = cv2.imread('data/'+str(count)+'.png')
-    crop_img1 = img[0:425, 0:950] # Le Dai Hanh 2
-    crop_img2 = img[0:425,950:1919] # Ly Thuong Kiet 2
-    crop_img3 = img[496:944, 0:942] #3 thang 2 Ly Thai To
-    crop_img4 = img[496:944, 954:1919] # 3 thang 2 Su van hanh
-    cv2.imwrite('23092021/1/' + str(count) + '.png', crop_img1)
-    cv2.imwrite('23092021/2/' + str(count) + '.png', crop_img2)
-    cv2.imwrite('23092021/3/' + str(count) + '.png', crop_img3)
-    cv2.imwrite('23092021/4/' + str(count) + '.png', crop_img4)
-    count+=1
+for f in glob.glob("data/*.png"):
+    img = cv2.imread(f)
+    filename = f[5:]
+    crop_img1 = img[110:510, 0:972] # Le Dai Hanh 1
+    crop_img2 = img[110:510,975:1916] # Ly Thuong Kiet 2
+    crop_img3 = img[590:1080, 5:972] #3 thang 2 Ly Thai To
+    crop_img4 = img[590:1080, 972:1919] # 3 thang 2 Su van hanh
+    cv2.imwrite('27012022/LDH1/' + filename, crop_img1)
+    cv2.imwrite('27012022/LDH2/' + filename, crop_img2)
+    cv2.imwrite('27012022/LTK2/' + filename, crop_img3)
+    cv2.imwrite('27012022/LTK3/' + filename, crop_img4)
+            
